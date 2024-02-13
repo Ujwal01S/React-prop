153eae950daa186ed0cdbd0c6b6d53a1dd8907ab
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-02-13 14:51:36 +0545

    1st

--- src/App.js
+++ src/App.js
@@ -24,9 +24,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHanddler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
+
+
   return(
     <div>
-    <NewExpenses />
+    <NewExpenses
+    onAddExpense = {addExpenseHanddler} />
     <Expenses 
     items = {expenses}
     />
